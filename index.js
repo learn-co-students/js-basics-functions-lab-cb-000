d6ccb669707bf52ce072f3bcfa6d556c99c8aec5
Jen Poole <contact.vandepool@gmail.com>
2018-04-21 22:19:43 +0000

    calculated distance from HQ

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution in this file!
+// returns a distance in blocks
+// calculates distances below 42nd street
+function distanceFromHqInBlocks(blockNumber ) {
+  if (blockNumber > 42) {
+    return blockNumber - 42;
+  } else {
+    return 42 - blockNumber;
+  }
+}
+
+// returns a distance in feet
+// calculates distances below 42nd street
+function distanceFromHqInFeet(blockNumber) {
+  return distanceFromHqInBlocks(blockNumber) * 264;
+}
