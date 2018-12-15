c2a81991e32fba4d84347c1d66a97d1fc73e3b5f
Elizabeth J <liz.jenerson@gmail.com>
2018-12-14 22:10:23 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blockNumber) {
+	let distance = parseInt(blockNumber, 10) - 42;
+	return distance < 0 ? Math.abs(distance) : distance;
+}
+
+function distanceTravelledInFeet(start, destination) {
+	let distance = parseInt(Math.abs(destination - start));
+	return (distance) * 264;
+}
+
+function distanceFromHqInFeet(blockNumber) {
+	return distanceFromHqInBlocks(blockNumber) * 264
+	//Returns the number of feet from Scuber's headquarters to the pickup location. 
+}
+
+function calculatesFarePrice(start, destination) {
+	let distance = distanceTravelledInFeet(start, destination);
+	let discountedDistance;
+	let fare;
+
+	if (distance < 400) {
+		return 0; 
+	} else if (distance >= 400 && distance < 2000) {
+		discountedDistance = distance - 400;
+		return fare = discountedDistance * .02;
+	} else if (distance > 2000 && distance < 2500) {
+		return 25;
+	} else {
+		return 'cannot travel that far';
+	}
+}
