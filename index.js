d1ccce11199149883382ff95b69a1ef92989989e
Will Figueiredo <will@wfigueiredo.com>
2019-03-29 01:52:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+  if (block > 42) {
+    return block - 42;
+  }
+  if (block < 42) {
+    return 42 - block;
+  }
+};
+function distanceFromHqInFeet(block) {
+  return distanceFromHqInBlocks(block) * 264;
+};
+function distanceTravelledInFeet(start, destination) {
+  if (start < destination) {
+    return (destination - start) * 264;
+  }
+  if (destination - start) {
+    return (start - destination) * 264;
+  }
+};
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+  if (distance <= 400) {
+    return 0;
+  } else if (distance >= 400 && distance <= 2000) {
+    return .02 * (distance - 400);
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+};
