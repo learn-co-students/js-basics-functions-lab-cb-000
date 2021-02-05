7d97785726c1389483e804e6bbd303b5c417dc66
Emma Konrad <emma.konrad92@gmail.com>
2021-02-05 03:58:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickup) {
+  if (pickup > 42) {
+    return pickup - 42;
+  } else {
+    return 42 - pickup;
+  }
+}
+
+function distanceFromHqInFeet(pickup) {
+  return distanceFromHqInBlocks(pickup)*264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  if (start > destination) {
+    return (start - destination) * 264;
+} else {
+    return (destination - start) *264;
+}
+    
+  }
+
+function calculatesFarePrice(start, destination) {
+  if (distanceTravelledInFeet(start, destination) < 400) {
+  return 0;
+  } else if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) < 2000) {
+    return (distanceTravelledInFeet(start, destination) - 400) * 0.02;
+  } else if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) < 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+ }  
+ 
